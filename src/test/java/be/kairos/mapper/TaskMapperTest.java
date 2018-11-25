fec39cf896b90072ee712e5f7a8120624dd38725
Kim Lauwers <lauwers.kim@gmail.com>
2018-11-25 20:25:45 +0100

    TaksMapperTest

--- src/test/java/be/kairos/mapper/TaskMapperTest.java
+++ src/test/java/be/kairos/mapper/TaskMapperTest.java
@@ -3,12 +3,40 @@ package be.kairos.mapper;
 import be.kairos.domain.Task;
 import be.kairos.domain.TaskAssert;
 import be.kairos.representation.TaskR;
+import be.kairos.representation.TaskRAssert;
 import org.junit.Test;
 
+import static be.kairos.domain.TaskTestBuilder.defaultTask;
 import static be.kairos.representation.TaskRTestBuilder.defaultTaskR;
 
 public class TaskMapperTest {
     final Mapper<Task, TaskR> mapper = new TaskMapper();
+
+    @Test
+    public void mapToRepresentation() {
+        final Task task = defaultTask().build();
+
+        final TaskR taskR = mapper.mapToRepresenation(task);
+
+        new TaskRAssert(taskR)
+                .hasId(task.getId())
+                .hasTitle(task.getTitle())
+                .hasNotes(task.getNotes());
+    }
+
+    @Test
+    public void mapToRepresentationNullNotes() {
+        final Task task = defaultTask()
+                .withNotes(null)
+                .build();
+
+        final TaskR taskR = mapper.mapToRepresenation(task);
+
+        new TaskRAssert(taskR)
+                .hasId(task.getId())
+                .hasTitle(task.getTitle())
+                .hasNoNotes();
+    }
 
     @Test
     public void mapToDomain() {
---
+++ src/test/java/be/kairos/representation/TaskRAssert.java
@@ -0,0 +1,35 @@
+package be.kairos.representation;
+
+import org.assertj.core.api.AbstractAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TaskRAssert extends AbstractAssert<TaskRAssert, TaskR> {
+    private final TaskR taskR;
+
+    public TaskRAssert(final TaskR taskR) {
+        super(taskR, TaskRAssert.class);
+        assertThat(taskR).isNotNull();
+        this.taskR = taskR;
+    }
+
+    public TaskRAssert hasId(final Long id) {
+        assertThat(this.taskR.getId()).isNotNull().isEqualTo(id);
+        return this;
+    }
+
+    public TaskRAssert hasTitle(final String title) {
+        assertThat(this.taskR.getTitle()).isNotNull().isEqualTo(title);
+        return this;
+    }
+
+    public TaskRAssert hasNotes(final String notes) {
+        assertThat(this.taskR.getNotes()).isNotNull().isEqualTo(notes);
+        return this;
+    }
+
+    public TaskRAssert hasNoNotes() {
+        assertThat(this.taskR.getNotes()).isNull();
+        return this;
+    }
+}
