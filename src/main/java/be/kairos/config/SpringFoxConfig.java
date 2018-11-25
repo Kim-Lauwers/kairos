d157f2fa1df1f54b3c50840c77cee266e9cc45bb
Kim Lauwers <lauwers.kim@gmail.com>
2018-11-25 17:40:48 +0100

    Static import

--- src/main/java/be/kairos/config/SpringFoxConfig.java
+++ src/main/java/be/kairos/config/SpringFoxConfig.java
@@ -10,11 +10,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
 @EnableSwagger2
@@ -24,7 +25,7 @@ public class SpringFoxConfig {
 
     @Bean
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("be.kairos"))
                 .paths(PathSelectors.ant("/api/**"))
